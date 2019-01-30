7474e80b6a8c6366b7303bcd1cda2f2a34212888
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-01-30 14:46:01 -0400

    Added markup for links on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,34 +1,42 @@
-import { Link } from 'gatsby'
 import PropTypes from 'prop-types'
 import React from 'react'
+import { Link } from 'gatsby'
+import styled from 'styled-components'
+import bg from '../images/background_blue.jpg'
+
+const Wrapper = styled.div`
+  background-image: url(${ bg });
+  height: 100vh;
+  background-size: cover;
+  position: relative;
+`
+
+const StyledLink = styled(Link)`
+  color: white;
+  text-transform: uppercase;
+  font-weight: 800;
+`
+
+const Ul = styled.ul`
+  position: absolute;
+  right: 2rem;
+  top: 2rem;
+`
+
+const Li = styled.li`
+  display: inline-block;
+  margin-right: 1rem;
+`
 
 const Header = ({ siteTitle }) => (
-  <div
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </div>
+  <Wrapper>
+    <Ul>
+      <Li><StyledLink to="">Home</StyledLink></Li>
+      <Li><StyledLink to="">About</StyledLink></Li>
+      <Li><StyledLink to="">Works</StyledLink></Li>
+      <Li><StyledLink to="">Contact</StyledLink></Li>
+    </Ul>
+  </Wrapper>
 )
 
 Header.propTypes = {
