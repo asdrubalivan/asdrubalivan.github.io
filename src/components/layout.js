c73e688a87df8073ef44996e54403424b9bd9d08
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-02-08 13:49:17 -0400

    Removed layout css

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { StaticQuery, graphql } from 'gatsby'
-import { createGlobalStyle } from 'styled-components'
 import Header from './header'
-import './layout.css'
-import woff from '../fonts/century_gothic/gothic.woff';
-
-const GlobalStyle = createGlobalStyle`
-  *, *::after, *::before {
-      margin: 0;
-      padding: 0;
-      box-sizing: inherit;
-      font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif;
-      src: url(${woff});
-  }
-  body {
-    box-sizing: border-box;
-  }
-  html {
-    font-size: 56.25%;
-  }
-`
+import GlobalStyle from './globalstyle'
 
 const Layout = ({ children }) => (
   <StaticQuery
