fb13aa4bb678eb0fe7eba02b08e2f15a21305187
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-02-08 13:38:54 -0400

    Added fonts

--- src/components/layout.js
+++ src/components/layout.js
@@ -4,12 +4,15 @@ import { StaticQuery, graphql } from 'gatsby'
 import { createGlobalStyle } from 'styled-components'
 import Header from './header'
 import './layout.css'
+import woff from '../fonts/century_gothic/gothic.woff';
 
 const GlobalStyle = createGlobalStyle`
   *, *::after, *::before {
       margin: 0;
       padding: 0;
       box-sizing: inherit;
+      font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif;
+      src: url(${woff});
   }
   body {
     box-sizing: border-box;
