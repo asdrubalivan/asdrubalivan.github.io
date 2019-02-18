b30d3d18363a10474098cfe391898b47d80b8fad
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-02-18 15:22:02 -0400

    Changed theme colors

--- src/components/layout.js
+++ src/components/layout.js
@@ -6,7 +6,9 @@ import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './globalstyle'
 
 const theme = {
-  darkBlue: '#311122'
+  darkBlue: '#152F4A',
+  gray: '#7D8899',
+  lightGray: '#F0F1F3',
 }
 
 const Layout = ({ children }) => (
