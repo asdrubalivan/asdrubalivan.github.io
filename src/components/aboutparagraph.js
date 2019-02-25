43126e548ba50d8a03de2901ed72495d3764419f
asdrubalivan <asdrubal.ivan.suarez.rivera@gmail.com>
2019-02-25 13:41:17 -0400

    Adding styles for AboutParagraph component

--- src/components/aboutparagraph.js
+++ src/components/aboutparagraph.js
@@ -3,12 +3,19 @@ import styled from 'styled-components'
 
 const Title = styled.h2`
   color: ${props => props.theme.darkBlue};
-  font-size: 3rem;
+  font-size: 4rem;
   text-transform: uppercase;
+  text-align: center;
+  margin-top: 8rem;
+  margin-bottom: 5rem;
+  font-weight: 300;
 `
 
 const Text = styled.p`
-  color: red;
+  color: ${props => props.theme.darkBlue};
+  text-align: center;
+  font-size: 2rem;
+  font-weight: 100;
 `
 
 const AboutParagraph = ({ children, title }) => (
